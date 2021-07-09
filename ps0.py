de35eafdcffcc4f3facf0490c8766097a31b5b24
KavitaDoobay <66803220+KavitaDoobay@users.noreply.github.com>
2021-07-09 22:02:44 +0000

    Completed Assignment

--- ps0.py
+++ ps0.py
@@ -1,10 +1,14 @@
+import math
 # 1. Ask the user to enter a number "x"
+x=int(input("Enter number x: "))
 
 
 # 2. Ask the user to enter a number "y"
-
+y=int(input("Enter number y: "))
 
 # 3. Prints out the number "x" raised to the power "y"
-
+p = x**y
+print("x**y = " + str(p))
 
 # 4. Prints out the log (base 2) of "x"
+print("log(x) = " + str(math.log2(x)))
